000badc5764965927452bbfe044b675316250cdc
mrpauloen <69369238+mrpauloen@users.noreply.github.com>
2021-02-12 20:35:16 +0100

    Update

--- inc/activate.php
+++ inc/activate.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package  AlecadddPlugin
+ * 
  */
 
 class WPFreeBotActivate
--- wp-free-bot.php
+++ wp-free-bot.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Plugin name: WP Free Bot
- * Description: Autoresponder i newsletter plugin
+ * Description: Zaawansowany autoresponder i newsletter w jednym
  * Author: Paweł Nowak
  * Author URI: https://github.com/mrpauloen/
  */
- 
+
  if ( ! defined( 'ABSPATH' ) ){
 	die;
  }
- 
+
   if ( class_exists( 'WPFreeBot' ) ) {
 
  class WPFreeBot
@@ -17,14 +17,33 @@
 
 	 function register() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
-	 }
+
+		add_action( 'admin_menu', array( $this, 'add_admin_pages') );
+	}
+
+	public function add_admin_pages(){
+		add_menu_page(
+			'WPFreeBot',
+			'WP Free Bot',
+			'manage_options',
+			'wp_free_bot',
+			array( $this, 'admin_index' ),
+			'dashicons-buddicons-replies',
+			6
+		);
+	}
+
+	public function admin_index(){
+		require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+
+	}
 
 	protected function create_post_type() {
 		add_action( 'init', array( $this, 'custom_post_type' ) );
 	}
-	 
+
 	 function custom_post_type(){
-		 
+
 		 $args = array(
 			'public' => true,
 			'label' => 'Newsletter',
@@ -44,7 +63,7 @@
 		WPFreeBotActivate::activate();
 	}
  }
- 
+
 
 	 $wpFreeBot = new WPFreeBot();
 	 $wpFreeBot->register();
