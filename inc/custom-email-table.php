3f8936374486191ae4541c1863afd0b281d54409
Paweł <69369238+mrpauloen@users.noreply.github.com>
2023-04-02 18:16:27 +0200

    Update custom-email-table.php

--- inc/custom-email-table.php
+++ inc/custom-email-table.php
@@ -1,4 +1,18 @@
-<?phpglobal $wpdb;
+<?php
+
+/**
+ *
+ * Here's a brief explanation of what this code does:
+ * The $wpdb global variable is used to access the WordPress database functions and properties.
+ * A table name is defined based on the WordPress database prefix and the desired name for the custom email table.
+ * The SHOW TABLES MySQL statement is used to check if the table already exists in the database.
+ * If the table does not already exist, an SQL query is defined for creating the custom email table. This example table includes columns for id, email_address, and created_at.
+ * The dbDelta function is used to execute the SQL query and create the table. This function is provided by WordPress to handle database schema updates and ensure compatibility across different versions of MySQL.
+ * You can modify the table structure and column names as needed to fit your specific use case. Once the table is created, you can use standard SQL queries to interact with it and store or retrieve email addresses as desired.
+ *
+*/
+
+global $wpdb;
 
 $table_name = $wpdb->prefix . 'custom_emails';
 
